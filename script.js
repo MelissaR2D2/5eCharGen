37b9d596d13309e9adb55718d894d239c29f4767
Tyla Merrill <tylamerrill@gmail.com>
2019-10-09 16:17:12 +0000

    Added races and dropdown

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var app = new Vue({
     el: '#app',
     data: {
-        myClasses: [],
+        classNames: [],
+        raceNames: [],
+        classURLs: [],
+        raceURLs: [],
+        classSelection: "",
+        raceSelection: "",
+        cors: "https://cors-anywhere.herokuapp.com/"
     },
-    methods: {
-        fetchREST() {
-            var cors = "https://cors-anywhere.herokuapp.com/";
-            const url = cors + "http://dnd5eapi.co/api/classes/";
+    computed: {
+        fetchClasses() {
+            const url = this.cors + "http://dnd5eapi.co/api/classes/";
             fetch(url)
                 .then((response) => {
                     return (response.json());
@@ -16,7 +21,24 @@ var app = new Vue({
                     console.log(json.results);
                     for (let i = 0; i < json.results.length; i++) {
                         console.log(json.results[i]);
-                        this.myClasses.push(json.results[i]);
+                        this.classNames.push(json.results[i].name);
+                        this.classURLs.push(json.results[i].url);
+                    }
+                });
+        },
+        fetchRaces() {
+            const url = this.cors + "http://dnd5eapi.co/api/races/";
+            fetch(url)
+                .then((response) => {
+                    return (response.json());
+                })
+                .then((json) => {
+                    console.log(json);
+                    console.log(json.results);
+                    for (let i = 0; i < json.results.length; i++) {
+                        console.log(json.results[i]);
+                        this.raceNames.push(json.results[i].name);
+                        this.raceURLs.push(json.results[i].url);
                     }
                 });
         },
